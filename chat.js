722b33985eeb52b2801e7b0aa9df912eb469e520
Ilya Sidorchik <ilya@sidorchik.ru>
2020-08-19 22:47:25 +0300

    Rewrite chat from WS to Socket.io

--- chat.js
+++ chat.js
@@ -1,6 +1,5 @@
 'use strict';
 
-const WebSocket = require(`ws`);
 const express = require(`express`);
 const path = require(`path`);
 const http = require(`http`);
@@ -9,27 +8,23 @@ const PORT = 4000;
 
 const app = express();
 const server = http.createServer(app);
-const ws = new WebSocket.Server({ server });
+const io = require(`socket.io`)(server);
 
 app.get(`/`, async (req, res) => {
   res.sendFile(path.resolve(__dirname, `index.html`));
 });
 
-ws.on(`connection`, (socket, req) => {
-  const { remoteAddress: IP } = req.socket;
+io.on(`connection`, (socket) => {
+  const { address: IP } = socket.handshake;
   console.log(`Новое подключение: ${IP}`);
 
   socket.on(`message`, (clientMessage) => {
     console.log(`> ${clientMessage}`);
 
-    ws.clients.forEach((client) => {
-      if (client.readyState === WebSocket.OPEN && client !== socket) {
-        client.send(clientMessage);
-      }
-    });
+    socket.broadcast.emit(`message`, clientMessage);
   });
 
-  socket.on(`close`, () => {
+  socket.on(`disconnect`, () => {
     console.log(`Клиент отключён: ${IP}`);
   });
 
